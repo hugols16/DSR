0d46eab4d1674caad84b16e259809227b3bfa596
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-02-23 18:17:42 -0500

    DataManager skeleton

---
+++ dsr_arduino_code/source/DataManagement.cpp
@@ -0,0 +1,14 @@
+class DataManager {
+private:
+  //data members go here
+  // int sensorReading = 0;
+public:
+  //getters
+  // int getSensorReading() { return sensorReading; }
+
+  //updater
+  void update() {
+    //update new values
+    // sensorReading = newSensorReading;
+  }
+}
