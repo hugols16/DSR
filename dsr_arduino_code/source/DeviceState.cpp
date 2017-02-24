aaf47ca51a16c0ba3ce86ccb6a82e209df494801
Pavel Shering <pshering@Pavels-MacBook-Pro.local>
2017-02-24 01:28:35 -0500

    readability change

--- dsr_arduino_code/source/DeviceState.cpp
+++ dsr_arduino_code/source/DeviceState.cpp
@@ -1,4 +1,14 @@
-enum STATES { INIT, READY, RAMP_SEARCH, RAMP_UP, RAMP_LEVEL, RAMP_DOWN, SEARCHING, FOUND_BASE, DONE };
+enum STATES {
+    INIT,
+    READY,
+    RAMP_SEARCH,
+    RAMP_UP,
+    RAMP_LEVEL,
+    RAMP_DOWN,
+    SEARCHING,
+    FOUND_BASE,
+    DONE
+};
 
 class DeviceState {
 public:
