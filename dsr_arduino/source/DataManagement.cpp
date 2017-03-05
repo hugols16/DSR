afd208c222961cfa41fbfe17f1795d67c42b43ec
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-03-04 19:05:02 -0500

    DataManager US reading basic

--- dsr_arduino/source/DataManagement.cpp
+++ dsr_arduino/source/DataManagement.cpp
@@ -1,14 +1,36 @@
+#include "SensorReading.cpp"
+// #include <queue>
+struct DataValue {
+  float value = 0;
+  // int arr = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+};
+
 class DataManager {
 private:
   //data members go here
   // int sensorReading = 0;
+  static DataValue usFront;
+  static DataValue usLeft;
+  static DataValue usRight;
+
 public:
   //getters
   // int getSensorReading() { return sensorReading; }
+  float getUsLeft() { return usLeft.value; }
+  float getUsRight() { return usRight.value; }
+  float getUsFront() { return usFront.value; }
 
   //updater
   void update() {
     //update new values
     // sensorReading = newSensorReading;
+
+    usLeft.value = sensorReadingLeft();
+    usRight.value = sensorReadingRight();
+    usFront.value = sensorReadingFront();
   }
-}
+};
+
+DataValue DataManager::usFront;
+DataValue DataManager::usLeft;
+DataValue DataManager::usRight;
