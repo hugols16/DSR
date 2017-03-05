616175b5ecd38b9db90e3b11c9b1253fb17b29d5
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-03-04 20:50:37 -0500

    DataManagement US reading stable

--- dsr_arduino/source/DataManagement.cpp
+++ dsr_arduino/source/DataManagement.cpp
@@ -1,36 +1,54 @@
 #include "SensorReading.cpp"
-// #include <queue>
-struct DataValue {
-  float value = 0;
-  // int arr = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-};
 
 class DataManager {
 private:
-  //data members go here
-  // int sensorReading = 0;
-  static DataValue usFront;
-  static DataValue usLeft;
-  static DataValue usRight;
+  static int usFront;
+  static int frontDiff;
+  static int usLeft;
+  static int leftDiff;
+  static int usRight;
+  static int rightDiff;
 
 public:
-  //getters
-  // int getSensorReading() { return sensorReading; }
-  float getUsLeft() { return usLeft.value; }
-  float getUsRight() { return usRight.value; }
-  float getUsFront() { return usFront.value; }
+  int getUsFront() { return usFront; }
+  int getUsLeft() { return usLeft; }
+  int getUsRight() { return usRight; }
 
   //updater
   void update() {
-    //update new values
-    // sensorReading = newSensorReading;
+    int frontReading = (int) sensorReadingFront();
+    int leftReading = (int) sensorReadingLeft();
+    int rightReading = (int) sensorReadingRight();
 
-    usLeft.value = sensorReadingLeft();
-    usRight.value = sensorReadingRight();
-    usFront.value = sensorReadingFront();
+    if ( frontReading != usFront) {
+      frontDiff++;
+    } else { frontDiff = 0; }
+    if( frontDiff >= 10 ) {
+      usFront = frontReading;
+      frontDiff = 0;
+    }
+
+    if (leftReading != usLeft) {
+      leftDiff++;
+    } else { leftDiff = 0; }
+    if ( leftDiff >= 10 ) {
+      usLeft = leftReading;
+      leftDiff = 0;
+    }
+
+    if (rightReading != usRight) {
+      rightDiff++;
+    } else { rightDiff = 0; }
+    if ( rightDiff >= 10 ) {
+      usRight = rightReading;
+      rightDiff = 0;
+    }
   }
 };
 
-DataValue DataManager::usFront;
-DataValue DataManager::usLeft;
-DataValue DataManager::usRight;
+int DataManager::usFront = 0;
+int DataManager::frontDiff = 0;
+int DataManager::usLeft = 0;
+int DataManager::leftDiff = 0;
+int DataManager::usRight = 0;
+int DataManager::rightDiff = 0;
