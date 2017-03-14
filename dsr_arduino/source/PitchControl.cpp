81dd8eefba88d8801bbbe61c43db572436b6e45f
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-03-14 18:24:13 -0400

    Fix PitchControl

--- dsr_arduino/source/PitchControl.cpp
+++ dsr_arduino/source/PitchControl.cpp
@@ -1,19 +1,24 @@
 #pragma once
 #include "DataManagement.cpp"
-#include "Defines.cpp"
+#include "Defines.h"
 
 void waitFor(int dir, int deg, DataManager * dm) {
   float current_deg = 0;
+  int t1 = micros(), t2;
 
   if(dir == UP) {
     while(current_deg < deg) {
+      t2 = t1;
+      t1 = micros();
       dm->updateGyro();
-      current_deg += dm->getGyroY();
+      current_deg += dm->getGyroY() * (t1 - t2) * 0.000001;
     }
   } else if (dir == DOWN) {
     while (current_deg > deg) {
+      t2 = t1;
+      t1 = micros();
       dm->updateGyro();
-      current_deg += dm->getGyroY();
+      current_deg += dm->getGyroY() * (t1 - t2) * 0.000001;
     }
   } else {
     Serial.println("Inavlid waitFor Heading direction");
