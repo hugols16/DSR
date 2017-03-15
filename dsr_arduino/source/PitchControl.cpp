63a45ad253ad6bde93dbfd350ed8a2a749461e6a
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-03-15 19:10:13 -0400

    Fix pitch control

--- dsr_arduino/source/PitchControl.cpp
+++ dsr_arduino/source/PitchControl.cpp
@@ -4,16 +4,21 @@
 
 void waitFor(int dir, int deg, DataManager * dm) {
   float current_deg = 0;
+  unsigned long t1 = micros(), t2;
 
   if(dir == UP) {
-    while(current_deg < deg) {
+    while(current_deg > -deg) {
+      t2 = t1;
+      t1 = micros();
       dm->updateGyro();
-      current_deg += dm->getGyroY();
+      current_deg += dm->getGyroY() * (t1 - t2) * 0.000001;
     }
   } else if (dir == DOWN) {
-    while (current_deg > deg) {
+    while (current_deg < deg) {
+      t2 = t1;
+      t1 = micros();
       dm->updateGyro();
-      current_deg += dm->getGyroY();
+      current_deg += dm->getGyroY() * (t1 - t2) * 0.000001;
     }
   } else {
     Serial.println("Invalid waitFor Heading direction");
