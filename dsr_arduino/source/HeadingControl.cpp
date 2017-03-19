7bdfe3a92fc65c9bf2aff4a443a8b4a373607257
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-03-18 23:08:30 -0400

    Add sigmoid motor control based on heading

--- dsr_arduino/source/HeadingControl.cpp
+++ dsr_arduino/source/HeadingControl.cpp
@@ -8,6 +8,7 @@ private:
   static DataManager dm_m;
 public:
   static float heading;
+  static float target;
 
   HeadingReader () {
     DataManager dm_m;
@@ -19,6 +20,9 @@ public:
     if (t2 - t1 < 100000) {
       dm_m.updateGyro();
       heading += dm_m.getGyroZ() * (t2 - t1) * 0.000001;
+      // Sigmoid function
+      float multiplier = 2.0 / (1.0 + pow(2, (-0.75 * abs(heading)))) - 0.5 * heading > 0 ? 1.0 : -1.0;
+      move(-MAX_SPEED_RIGHT * (0.8 - 0.2 * multiplier), -MAX_SPEED_LEFT * (0.8 + 0.2 * multiplier), 1);
     }
 
     t1 = t2;
@@ -29,3 +33,4 @@ unsigned long HeadingReader::t1 = 0;
 unsigned long HeadingReader::t2 = 0;
 DataManager HeadingReader::dm_m;
 float HeadingReader::heading = 0;
+float HeadingReader::target = 0;
